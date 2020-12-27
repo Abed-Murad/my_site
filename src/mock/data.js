306143eff64540ff57750b88151d27a769611aa3
Abed Murad <abed.murad@protonmail.com>
2020-12-27 17:17:14 +0200

    Update

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,11 +19,11 @@ export const heroData = {
 export const aboutData = {
   img: 'canvas.png',
   paragraphOne:
-    "I'm a Google-certified Android Engineer, Who provides solutions based on advanced CS knowledge.",
+    "I'm a Google-certified Android Engineer, 4 years of experience leveraging Kotlin & Java to produce robust, well-architected apps based on advanced CS knowledge.",
   paragraphTwo:
-    'I hold over 4 years of experience leveraging Kotlin & Java to produce robust, well-architected apps.',
+    'My main interests are: Software architecture, Comparative Programming, Running and entrepreneurship.',
   paragraphThree:
-    'I like building new stuff and work with other people – nothing really interesting is ever built alone!',
+    'My work philosophy: Communication and collaboration with others is the key to great work, nothing really interesting is ever built alone!',
   resume: 'https://www.linkedin.com/in/abed-murad/', // if no resume, the button will not show up
 };
 
@@ -32,9 +32,10 @@ export const projectsData = [
   {
     id: nanoid(),
     img: 'homoola.png',
-    title: 'Homoola Captain',
-    info: 'Homoola is a state of the art transportation platform that provides its clients',
-    info2: 'with the perfect solution for all their land transportation needs in one place.',
+    title: 'Homoola ',
+    info: 'Homoola Captain for Trucks.',
+    info2:
+      'A state of the art transportation platform that provides its clients with the perfect solution for all their land transportation needs in one place.',
     url: 'https://play.google.com/store/apps/details?id=com.Homoola.HomoolaCaptain&hl=en',
     repo: '', // if no repo, the button will not show up
   },
@@ -51,9 +52,9 @@ export const projectsData = [
     id: nanoid(),
     img: 'youth_council.png',
     title: 'Youth Council',
-    info: 'An internal communication app for the local Youth councils',
+    info: 'An internal communication app for the local Youth councils.',
     info2:
-      'A way to send and receive data vertically and horizontally inside the councils hierarchical structure',
+      'A way to send and receive data vertically and horizontally inside the councils hierarchical structure.',
     url: 'https://play.google.com/store/apps/details?id=org.syfpal.application',
     repo: '', // if no repo, the button will not show up
   },
@@ -61,9 +62,9 @@ export const projectsData = [
     id: nanoid(),
     img: 'carly.png',
     title: 'Carly',
-    info: 'A peer-to-peer car sharing marketplace ',
+    info: 'A peer-to-peer car sharing marketplace.',
     info2:
-      'Carly helps cars owners to hearn some extra money by ranting there cars to tenants in prices 30% lower than the car rental agencies',
+      'Carly helps cars owners to hearn some extra money by ranting there cars to tenants in prices 30% lower than the car rental agencies.',
     url: 'https://github.com/Abed-Murad/carly',
     repo: '', // if no repo, the button will not show up
   },
@@ -71,7 +72,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'ya_msahel.png',
     title: 'Ya Msahel',
-    info: 'YaMsahel An online accommodation booking',
+    info: 'YaMsahel An online accommodation booking.',
     info2:
       'YaMsahel is a clone of Yamsafer, an online booking platform for flights tickets and hotel rooms.',
     url: 'https://github.com/Abed-Murad/YaMsahel',
@@ -82,9 +83,9 @@ export const projectsData = [
     img: 'movies_app.png',
     title: 'Popular Movies',
     info:
-      'A place where you can discover the most popular, the most rated or the highest rated movies',
+      'A place where you can discover the most popular, the most rated or the highest rated movies.',
     info2:
-      'Save favorite movies locally to view them even when offline, watch trailers and Read reviews',
+      'Save favorite movies locally to view them even when offline, watch trailers and Read reviews.',
     url: 'https://github.com/Abed-Murad/ND_Popular-Movies',
     repo: '', // if no repo, the button will not show up
   },
@@ -104,11 +105,6 @@ export const footerData = {
       id: nanoid(),
       name: 'twitter',
       url: 'https://twitter.com/Abed__Murad',
-    },
-    {
-      id: nanoid(),
-      name: 'instagram',
-      url: 'https://www.instagram.com/abed__murad/',
     },
     {
       id: nanoid(),
